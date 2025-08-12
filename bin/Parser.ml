d2aad0e5dd53eaf2c9b75872c65a8a661318e052
DanielBrv <daniel.bravop@pm.me>
2025-08-12 13:13:25 -0400

    Implemented parse_cnf

--- bin/Parser.ml
+++ bin/Parser.ml
@@ -23,61 +23,22 @@ let parse_literal toks =
     | Tok_Not :: Tok_Var(x)::t -> Not(Var(x)), t
     | _ -> raise (Failure "Parsing literals failure")
 
-let  parse_disjunct toks = 
+let rec parse_disjunct toks = 
   let literal, left_over = parse_literal toks in 
     match literal, left_over with
-    | Var(x), [] -> Lit(literal), []
-    | _, _ -> "c"
+    | Var(_), [] -> Lit(literal), []
+    | Not(_), [] -> Lit(literal), []
+    | _, Tok_Or::t -> let right_side, left_over = parse_disjunct t in
+                        Or(literal, right_side), left_over
+    | _, _ -> raise (Failure "Parsing disjunction failure")
 let rec parse_cnf toks = 
   let disjunction, left_over = parse_disjunct toks in 
     match disjunction, left_over with
-
-    | _, _ -> "c"
-
-
-
-(* let rec parse_literal toks = 
-  match toks with
-  | [] -> raise (Failure "parse error, No Tokens")
-  | Tok_Not :: t -> 
-    let sub_tree, left_over = parser t in
-      Not(sub_tree), left_over
-  | Tok_LParen :: Tok_Var(x) :: Tok_And :: Tok_Var(z) :: Tok_RParen :: t ->
-      Clause(Lit(x), AND, Lit(z)), t
-  | Tok_LParen :: Tok_Var(x) :: Tok_Or :: Tok_Var(z) :: Tok_RParen :: t ->
-    Clause(Lit(x), OR, Lit(z)), t
-  | Tok_LParen :: Tok_Var(x) :: Tok_RParen :: t -> 
-      op_parse t SubFormula(Lit(x))
-  | Tok_LParen :: t ->
-    let sub_tree, left_over = parser t in
-        op_parse left_over sub_tree (*replace function call with code its self*)
-  | Tok_Var(x) :: Tok_RParen :: t ->
-      Lit(x), t
-  | Tok_Var(x) :: [] -> Lit(x)
-  | Tok_Var(x) :: t -> op_parse t Lit(x)
-*)
-(*
-let rec parser toks = 
-  match toks with
-  | [] -> raise (Failure "parse error, No Tokens")
-  | Tok_Not :: t -> 
-    let sub_tree, left_over = parser t in
-      Not(sub_tree), left_over
-  | Tok_LParen :: Tok_Var(x) :: Tok_And :: Tok_Var(z) :: Tok_RParen :: t ->
-      Clause(Lit(x), AND, Lit(z)), t
-  | Tok_LParen :: Tok_Var(x) :: Tok_Or :: Tok_Var(z) :: Tok_RParen :: t ->
-    Clause(Lit(x), OR, Lit(z)), t
-  | Tok_LParen :: Tok_Var(x) :: Tok_RParen :: t -> 
-      op_parse t SubFormula(Lit(x))
-  | Tok_LParen :: t ->
-    let sub_tree, left_over = parser t in
-        op_parse left_over sub_tree (*replace function call with code its self*)
-  | Tok_Var(x) :: Tok_RParen :: t ->
-      Lit(x), t
-  | Tok_Var(x) :: [] -> Lit(x)
-  | Tok_Var(x) :: t -> op_parse t Lit(x)
-  *)
-
+    | Lit(_), [] -> Clause(disjunction), []
+    | Or(_,_), [] -> Clause(disjunction), []
+    | _, Tok_And::t -> let right_side, left_over = parse_cnf t in
+                        And(disjunction, right_side), left_over
+    | _, _ -> raise (Failure "Parsing cnf failure")
 
 
 let parse toks =
