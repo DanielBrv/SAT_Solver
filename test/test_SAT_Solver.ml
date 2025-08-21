0e31f1f13bec918df82f6085b390fdb8e1329d46
DanielBrv <daniel.bravop@pm.me>
2025-08-21 14:45:30 -0400

    Added more unit tests

--- test/test_SAT_Solver.ml
+++ test/test_SAT_Solver.ml
@@ -1,28 +1,87 @@
 open Satsolver_lib.Types
 
-let test_parse_simple () =
-  let test1 = Satsolver_lib.Parser.parse (Satsolver_lib.Lexer.lexer "(a)") in
-  let tree1 = Clause(Lit(Var 'a')) in
-  assert (test1 = tree1)
+let test_parse_simple_clause_1 () =
+  let test = Satsolver_lib.Parser.parse (Satsolver_lib.Lexer.lexer "(a)") in
+  let tree = Clause(Lit(Var 'a')) in
+  assert (test = tree)
 
-let test_parse_not () =
-  let test2 = Satsolver_lib.Parser.parse (Satsolver_lib.Lexer.lexer "(~a)") in
-  let tree2 = Clause(Lit(Not (Var 'a'))) in
-  assert (test2 = tree2)
+let test_parse_simple_clause_2 () =
+  let test = Satsolver_lib.Parser.parse (Satsolver_lib.Lexer.lexer "(a)") in
+  let tree = Clause(Lit(Var 'b')) in
+  assert (test != tree)
 
-let test_parse_or () =
-  let test3 = Satsolver_lib.Parser.parse (Satsolver_lib.Lexer.lexer "(a | b)") in
-  let tree3 = Clause(Or(Var 'a', Lit(Var 'b'))) in
-  assert (test3 = tree3)
+let test_parse_simple_not_1 () =
+  let test = Satsolver_lib.Parser.parse (Satsolver_lib.Lexer.lexer "(~a)") in
+  let tree = Clause(Lit(Not (Var 'a'))) in
+  assert (test = tree)
 
-let test_parse_and () =
-  let test4 = Satsolver_lib.Parser.parse (Satsolver_lib.Lexer.lexer "(a | b) & (c)") in
-  let tree4 = And(Or(Var 'a', Lit(Var 'b')), Clause(Lit(Var 'c'))) in
-  assert (test4 = tree4)
+let test_parse_simple_not_2 () =
+  let test = Satsolver_lib.Parser.parse (Satsolver_lib.Lexer.lexer "(~a)") in
+  let tree = Clause(Lit(Var 'a')) in
+  assert (test != tree)
+
+let test_parse_or_1 () =
+  let test = Satsolver_lib.Parser.parse (Satsolver_lib.Lexer.lexer "(a | b)") in
+  let tree = Clause(Or(Var 'a', Lit(Var 'b'))) in
+  assert (test = tree)
+
+let test_parse_or_2 () =
+  let test = Satsolver_lib.Parser.parse (Satsolver_lib.Lexer.lexer "(~a | b)") in
+  let tree = Clause(Or(Not(Var 'a'), Lit(Var 'b'))) in
+  assert (test = tree)
+
+let test_parse_or_3 () =
+  let test = Satsolver_lib.Parser.parse (Satsolver_lib.Lexer.lexer "(a | ~b)") in
+  let tree = Clause(Or(Var 'a', Lit(Not(Var 'b')))) in
+  assert (test = tree)
+
+let test_parse_or_4 () =
+  let test = Satsolver_lib.Parser.parse (Satsolver_lib.Lexer.lexer "(~a | ~b)") in
+  let tree = Clause(Or(Not(Var 'a'), Lit(Not(Var 'b')))) in
+  assert (test = tree)
+
+let test_parse_and_1 () =
+  let test = Satsolver_lib.Parser.parse (Satsolver_lib.Lexer.lexer "(a) & (b)") in
+  let tree = And(Lit(Var 'a'), Clause(Lit(Var 'b'))) in
+  assert (test = tree)
+
+let test_parse_and_2 () =
+  let test = Satsolver_lib.Parser.parse (Satsolver_lib.Lexer.lexer "(~a) & (b)") in
+  let tree = And(Lit(Not(Var 'a')), Clause(Lit(Var 'b'))) in
+  assert (test = tree)
+
+let test_parse_and_3 () =
+  let test = Satsolver_lib.Parser.parse (Satsolver_lib.Lexer.lexer "(a) & (~b)") in
+  let tree = And(Lit(Var 'a'), Clause(Lit(Not(Var 'b')))) in
+  assert (test = tree)
+
+let test_parse_and_4 () =
+  let test = Satsolver_lib.Parser.parse (Satsolver_lib.Lexer.lexer "(~a) & (~b)") in
+  let tree = And(Lit(Not(Var 'a')), Clause(Lit(Not(Var 'b')))) in
+  assert (test = tree)
+
+
+let test_parse_and_5 () =
+  let test = Satsolver_lib.Parser.parse (Satsolver_lib.Lexer.lexer "(a | b) & (c)") in
+  let tree = And(Or(Var 'a', Lit(Var 'b')), Clause(Lit(Var 'c'))) in
+  assert (test = tree)
+
 
 let () =
-  test_parse_simple ();
-  test_parse_not ();
-  test_parse_or ();
-  test_parse_and ();
+  test_parse_simple_clause_1 ();
+  test_parse_simple_clause_2 ();
+  
+  test_parse_simple_not_1 ();
+  test_parse_simple_not_2 ();
+
+  test_parse_or_1 ();
+  test_parse_or_2 ();
+  test_parse_or_3 ();
+  test_parse_or_4 ();
+
+  test_parse_and_1 ();
+  test_parse_and_2 ();
+  test_parse_and_3 ();
+  test_parse_and_4 ();
+  test_parse_and_5 ();
   print_endline "All tests passed!"
